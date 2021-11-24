4460d0cbacf5a53909be9b574837956723c2f232
Hazard4233 <fenglong37@outlook.com>
2021-11-24 16:21:45 +0800

    starter project is correctly set

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./tailwind.css"
 const App = () => {
   return (
     <div className="App bg-[#eee] h-[100vh] flex items-center justify-center flex-col">
-      <h1 className="font-bold text-4xl text-[#333]">Hi 👋, Tailwind test.</h1>
-      <p className="text-sm font-italic m-[3px]">
+      <h1 className="font-bold text-4xl text-[#333]">Hi 👋, Eden Hazard!</h1>
+      <p className="text-sm font-italic font-semibold m-[3px] text-pink-500">
         Now you can use <code className="bg-[#ccc] p-[3px]">[]</code> to style
         your app
       </p>
